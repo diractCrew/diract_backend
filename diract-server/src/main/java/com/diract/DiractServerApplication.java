1955fb3068a2b934cd6be2adbaf2f79113cea9d6
Hyunho Kang <hhwj2270@gmail.com>
2025-12-31 19:31:42 +0900

    init: spring boot project setup

---
+++ diract-server/src/main/java/com/diract/DiractServerApplication.java
@@ -0,0 +1,13 @@
+package com.diract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiractServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiractServerApplication.class, args);
+    }
+
+}
---
+++ diract-server/src/test/java/com/diract/DiractServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiractServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
